283ed3e4fcaa239f61790c60fbf8f03cbcbf8d18
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-03-30 09:37:43 -0400

    Update FlxGroup.hx

--- source/flixel/group/FlxGroup.hx
+++ source/flixel/group/FlxGroup.hx
@@ -5,7 +5,8 @@ import flixel.FlxG;
 import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
 import flixel.util.FlxArrayUtil;
 import flixel.util.FlxDestroyUtil;
-import flixel.util.FlxSignal.FlxTypedSignal;
+import flixel.util.FlxSignal.*;
+import flixel.util.FlxSignal;
 import flixel.util.FlxSort;
 #if (haxe_ver >= 4.2)
 import Std.isOfType;
