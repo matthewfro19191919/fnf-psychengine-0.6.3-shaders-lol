5edbad3b1b14fec88e88da9dd8a5388f3e2cd74d
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-03-30 07:14:48 -0400

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -6,7 +6,7 @@ echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
 haxelib install lime 8.1.2
 haxelib install openfl 9.3.3
-haxelib install flixel 5.2.2
+haxelib install flixel 4.11.0
 haxelib install flixel-tools 1.5.1
 haxelib install flixel-ui 2.5.0
 haxelib install flixel-addons 2.10.0
