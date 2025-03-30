2c3390ed5bb9b8d35e0f780223bd524f9910b567
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-03-30 08:06:40 -0400

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -6,13 +6,13 @@ cd ..
 echo Makking the main haxelib and setuping folder in same time..
 mkdir ~/haxelib && haxelib setup ~/haxelib
 echo Installing dependencies...
-echo This might take a few moments depending on your internet speed.
+echo This might take a few moments depending on your internet speed
 haxelib install lime 8.1.2
 haxelib install openfl 9.3.3
-haxelib install flixel 4.11.0
+haxelib install flixel 5.6.1
+haxelib install flixel-addons 3.2.2
 haxelib install flixel-tools 1.5.1
 haxelib install flixel-ui 2.5.0
-haxelib install flixel-addons 2.10.0
 haxelib install hscript-iris 1.1.3
 haxelib install tjson 1.4.0
 haxelib install hxdiscord_rpc 1.2.4
@@ -31,4 +31,5 @@ haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c0
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
+
 echo Finished!
